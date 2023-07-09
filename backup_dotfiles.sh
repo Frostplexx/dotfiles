5801a66dacf8b4e64616b9d9c5cb3631426b8338
frostplexx <daniel.inama02@gmail.com>
2023-07-09 10:00:02 +0200

    Backup dotfiles

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -103,7 +103,7 @@ then
             echo "Do you want to add it to the .gitignore file (y), coninue (n) or abort (a)? (y/n/a)"
             read -r answer
             if [ "$answer" == "y" ]; then
-                echo -e "\nprivate.pem" >> .gitignore
+                echo -e "\n./private.pem" >> .gitignore
                 echo "Added private.pem to .gitignore"
             elif [ "$answer" == "a" ]; then
                 exit
