401b7c1dce8cd4ed2ae85ecb3a32b67bb4e84556
Daniel <daniel.inama02@gmail.com>
2024-02-14 12:03:22 +0100

    Backup dotfiles

---
+++ scripts/backup_or_restore_homebrew_packages.sh
@@ -0,0 +1,10 @@
+
+read -p "Do you want to back up homebrew packages (b),m restore them (r)?" -n 1 -r
+echo
+if [[ $REPLY =~ ^[bB]$ ]]; then
+    ./helpers/backup_homebrew_packages.sh -b
+elif [[ $REPLY =~ ^[rR]$ ]]; then
+    ./helpers/backup_homebrew_packages.sh -r
+elif [[ $REPLY =~ ^[qQ]$ ]]; then
+    exit
+fi
