691db5beff5a8c702dfa03fa4f7cb5ff56e4194d
Daniel <daniel.inama02@gmail.com>
2024-02-17 12:15:44 +0100

    Backup dotfiles

--- .config/tmux/scripts/tmux-cht.sh
+++ .config/tmux/scripts/tmux-cht.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-selected=`cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command | fzf`
+
+selected=`echo "$(curl -s "https://en.wikipedia.org/wiki/List_of_programming_languages" | awk 'BEGIN { RS="</li>"; FS="<li>"; } NR>1 {print "<li>" $2}' | ggrep -oP '<li><a href="\/wiki\/.*".*<\/a>' | sed -e 's/<[^>]*>//g')$(cat ~/.config/tmux/tmux-cht-command)"|tr '[:upper:]' '[:lower:]' |fzf`
+
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+if grep -qs "$selected" ~/.tmux-cht.command; then
+    tmux neww bash -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 fi
 
